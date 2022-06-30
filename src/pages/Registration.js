c583a5868bac32959155d33c0cad145ee89b30fb
Abdul Momin <66585914+abdulmomin956@users.noreply.github.com>
2022-06-30 13:11:16 +0600

    register function added

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form'
 import { Button } from 'react-bootstrap';
+import axios from '../api/axios';
 
 const Registration = () => {
-    const handleSubmit = (event) => {
+    const [user, setUser] = useState('')
+    const [pass, setPass] = useState('')
+
+    const handleSubmit = async (event) => {
         event.preventDefault();
+        console.log(user, pass);
+        try {
+            const response = await axios.post('/api/register',
+                JSON.stringify({ user, pass }),
+                {
+                    headers: { "Content-Type": "application/json" }
+                }
+            )
+            console.log(response);
+        }
+        catch (err) {
+            console.log(err);
+        }
+
     }
+
     return (
         <section className='container my-5'>
             <header className='text-center'>
@@ -14,12 +33,12 @@ const Registration = () => {
             <Form onSubmit={handleSubmit} className='mx-auto' style={{ width: '500px' }} >
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Username</Form.Label>
-                    <Form.Control type="text" placeholder="Username" autoComplete='off' />
+                    <Form.Control type="text" placeholder="Username" autoComplete='off' onChange={(e) => setUser(e.target.value)} />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password" placeholder="Password" onChange={(e) => setPass(e.target.value)} />
                 </Form.Group>
                 <Button variant="primary" type="submit">
                     Register
