87e55c7fb8a382a5ecbe0fc030182742be30dabe
Abdul Momin <66585914+abdulmomin956@users.noreply.github.com>
2022-06-30 19:35:54 +0600

    login route completed

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,115 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import axios from '../api/axios';
+import { Link } from 'react-router-dom'
+
+
 
 const Login = () => {
+    const userRef = useRef();
+    const errRef = useRef();
+
+    const [user, setUser] = useState('')
+
+    const [pass, setPass] = useState('')
+
+    const [errMsg, setErrMsg] = useState('')
+    const [success, setSuccess] = useState(false);
+
+    useEffect(() => {
+        userRef.current.focus();
+    }, [])
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        // console.log(user, pass);
+
+        try {
+            const response = await axios.post('/api/login',
+                JSON.stringify({ user, pass }),
+                {
+                    headers: { "Content-Type": "application/json" },
+                    withCredentials: true,
+                }
+            )
+            console.log(response);
+            const accessToken = response?.data?.accessToken;
+            const roles = response?.data?.roles;
+            // setAuth({ user, roles, accessToken });
+            setSuccess(true);
+            //clear state and controlled inputs
+            //need value attrib on inputs for this
+            setUser('');
+            setPass('');
+        }
+        catch (err) {
+            // console.log(err);
+            if (!err?.response) {
+                setErrMsg('No Server Response');
+            } else if (err.response?.status === 401) {
+                setErrMsg('Unauthorized');
+            }
+            else if (err.response?.status === 400) {
+                setErrMsg('Missing username or password')
+            } else {
+                setErrMsg('Login failed')
+            }
+            errRef?.current?.focus();
+        }
+
+    }
+
+    useEffect(() => {
+        setErrMsg('')
+    }, [pass, user])
+
     return (
-        <div>
-            This is login page
-        </div>
+        <>{success ? (
+            <section className='container my-5 mx-auto text-center'>
+                <h1>Successfully Login!</h1>
+                <p>
+                    Go to <Link to='/'>Homepage</Link>
+                </p>
+            </section>
+        )
+            :
+            <section className='container my-5'>
+                <header className='text-center'>
+                    <h1>Please Login</h1>
+                </header>
+                <Form onSubmit={handleSubmit} className='mx-auto' style={{ width: '500px' }} >
+                    {
+                        errMsg && <Form.Text>
+                            <h5 ref={errRef} className='text-danger'>{errMsg}</h5>
+                        </Form.Text>
+                    }
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Username:</Form.Label>
+                        <Form.Control
+                            ref={userRef}
+                            type="text"
+                            placeholder="Username"
+                            autoComplete='off'
+                            onChange={(e) => setUser(e.target.value)}
+                            required />
+
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password:</Form.Label>
+                        <Form.Control
+                            type="password"
+                            placeholder="Password"
+                            required
+                            onChange={(e) => setPass(e.target.value)} />
+
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Login
+                    </Button>
+                </Form>
+            </section>}
+        </>
     );
 };
 
